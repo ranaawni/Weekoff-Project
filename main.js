2345f976c952f2f7f2dc8f4a18d5929f2e2f8a54
Rana Alhmoud <ranaalhmoud92@gmail.com>
2020-10-12 23:57:30 +0300

    finish signin page and check the validation start with making the score for the memory card game

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 $( document ).ready(function() {
-  
+
+
   //In sign up form
 var newUser;
   $("#signUpbtn").on('click',function(){
@@ -29,22 +30,119 @@ var newUser;
 
  //In sign in form 
  // check the validation
+  var user1 = {
+   email : 'ranaalhmoud92@gmail.com',
+   password : 123456789
+};
 
- $("#loginbtn").click(function(){
-  var emailLog = $("#userNameLogin").val();
-  // console.log(emailLog);
-  var passLog = $("#passwordLogin").val();
-  var user = {
-    "email" :  emailLog,
-    "password" : passLog
-  };
+// var emailLog;
+// var passLog;
+// var result;
 
-  $.each(newUser,function(key,value){
-     
+  $('#passwordLogin').keypress(function(e) {
+    if (e.keyCode == 13 && this.value == "123456789" && $("#userNameLogin").val() == "ranaalhmoud92@gmail.com") {
+        $("#loginbtn").click(function(){
+          $("#logIn").attr('action','index.html')
+        })
+    }
+  });
 
-  })
- });
+  /////// For Index.html
+
+   $(".final").hide();
+
+    // $("#submitbtn").click(function(){
+       
+
+    //          if( $("#exampleInputEmail").val()=='admin@admin.com' && $("#exampleInputPassword").val()=='password') {
+    //                    window.open("cards.html");
+    //                  }
+    //              else {
+    //                 alert("Please try again");
+    //              }
+ 
+                 $("#logoutbtn").click(function() {
+                  
+                  $("#alogout").attr('href','signIn.html');
+                  $("#logIn")[0].reset();
+      
+                 
+            });
+      
+      
 
 
+var cards = document.querySelectorAll('.memory-card');
+var hasFlippedCard=false;
+
+var lockBoard=false;
+var firstCard;
+var secondCard;
+// var score = 0;
+var score = $("#score").html();
+
+function flipCard() {
+  if (lockBoard) return;
+  if(this===firstCard) return;
+  this.classList.add('flip');
+
+  if(!hasFlippedCard){
+     // first click
+  
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  } 
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+     checkForMatch();
+
+}
+function checkForMatch() {
+  var isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+     isMatch ? disableCards()  : unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  score++;
+  $("#score").html = score;
+restBoard();
+}
+
+function unflipCards() {
+lockBoard=true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+   restBoard();
+  }, 1500);
+
+
+}
+
+function restBoard(){
+  hasFlippedCard,lockBoard=false,false;
+  firstCard,secondCard=null,null;
+}
+
+
+(function shuffle(){
+    cards.forEach(card=>{
+let randomPos = Math.floor(Math.random()*12); 
+card.style.order=randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+  
 
 });
